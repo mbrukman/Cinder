c51d69dfdf6dce7f06b3e9313f7228d3bfff8074
Rich E <rich.eakin@gmail.com>
2015-05-22 21:46:34 -0400

    some windows sample fixes

--- samples/CaptureBasic/src/CaptureBasicApp.cpp
+++ samples/CaptureBasic/src/CaptureBasicApp.cpp
@@ -1,6 +1,7 @@
 #include "cinder/Cinder.h"
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/Capture.h"
 #include "cinder/Log.h"
